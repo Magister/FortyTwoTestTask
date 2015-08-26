30a0824aa5f95edfa62006860427c002d34e1699
Misha Padalka <misha.cn.ua@gmail.com>
2015-08-26 13:55:32 +0300

    ticket:1 pep8 fix

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class AppUser(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
