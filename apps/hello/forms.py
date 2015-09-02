83f2edda64d8c29c514dc78aa3dabf5d9a9979cd
Misha Padalka <misha.cn.ua@gmail.com>
2015-09-02 21:30:31 +0300

    ticket:5 add datepicker widget and use it for edit page

--- apps/hello/forms.py
+++ apps/hello/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from apps.hello.models import AppUser
+from apps.hello.widgets import DatePickerWidget
 
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = AppUser
         fields = '__all__'
+        widgets = {
+            'date_of_birth': DatePickerWidget,
+        }
 
     def __init__(self, *args, **kwargs):
         super(EditForm, self).__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
+            current_class = self.fields[field].widget.attrs.get('class', '')
+            self.fields[field].widget.attrs['class'] = (
+                current_class + ' form-control').strip()
---
+++ apps/hello/widgets.py
@@ -0,0 +1,23 @@
+from django.forms import widgets
+
+
+class DatePickerWidget(widgets.DateInput):
+
+    class Media:
+        js = (
+            'libs/jquery/dist/jquery.min.js',
+            'libs/jquery-ui/jquery-ui.min.js',
+            'hello/js/datepicker-widget.js',
+        )
+        css = {
+            'all': ('libs/jquery-ui/themes/humanity/jquery-ui.min.css',)
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(DatePickerWidget, self).__init__(*args, **kwargs)
+        current_class = self.attrs.get('class', '')
+        self.attrs['class'] = (current_class + ' date-picker').strip()
+
+    def render(self, name, value, attrs=None):
+        html = super(DatePickerWidget, self).render(name, value, attrs)
+        return html
