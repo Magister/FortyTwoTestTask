68c28500a97cac9f5382c6244980df2f8136ddbb
Misha Padalka <misha.cn.ua@gmail.com>
2015-09-03 20:22:00 +0300

    ticket:8 implement template tag and use it on main page

--- apps/hello/templatetags/edit_link.py
+++ apps/hello/templatetags/edit_link.py
@@ -1,8 +1,17 @@
 from django import template
+from django.core.urlresolvers import reverse
+from django.db.models import Model
 
 register = template.Library()
 
 
 @register.simple_tag
 def edit_link(obj):
-    return None
+    if isinstance(obj, Model):
+        admin_url = reverse(
+            'admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name),
+            args=(obj.pk,))
+        link_tag = '<a href="%s">admin</a>' % (admin_url,)
+    else:
+        link_tag = ''
+    return link_tag
